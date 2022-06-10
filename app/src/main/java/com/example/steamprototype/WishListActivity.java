926a6557595279b74dad4b5a03db5f1b8801a087
Khang-Le-Duy <khang.le.set17@eiu.edu.vn>
2022-06-10 11:05:35 +0700

    Cat

---
+++ app/src/main/java/com/example/steamprototype/WishListActivity.java
@@ -0,0 +1,43 @@
+package com.example.steamprototype;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.steamprototype.adapter.ListViewAdapter;
+import com.example.steamprototype.adapter.SliderAdapter;
+import com.example.steamprototype.data_op.UserLibraryStorage;
+import com.example.steamprototype.entity.Game;
+import com.example.steamprototype.entity.User;
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+
+public class WishListActivity extends AppCompatActivity {
+    Button btn_search;
+    EditText edt_search;
+    ListView listView;
+    ListViewAdapter listViewAdapter;
+    ArrayList<Game> gameArrayList;
+    User user;
+    UserLibraryStorage userLibraryStorage;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wishlist_activity);
+        init();
+        Intent intent = getIntent();
+        this.user = (User) intent.getSerializableExtra("user");
+    }
+    public void init() {
+        btn_search = (Button) findViewById(R.id.btn_wishsearch);
+        edt_search = (EditText) findViewById(R.id.edit_wishsearch);
+        listView = (ListView) findViewById(R.id.lstWishlist);
+    }
+}
