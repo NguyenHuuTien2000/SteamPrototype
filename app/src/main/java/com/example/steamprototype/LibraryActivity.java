fdd7d5d73a962611e9f7f0dbc71a6723b2faf961
Khang-Le-Duy <khang.le.set17@eiu.edu.vn>
2022-06-10 11:45:09 +0700

    WishListActivity fix?

--- app/src/main/java/com/example/steamprototype/LibraryActivity.java
+++ app/src/main/java/com/example/steamprototype/LibraryActivity.java
@@ -8,18 +8,15 @@ import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.steamprototype.adapter.ListViewAdapter;
-import com.example.steamprototype.adapter.SliderAdapter;
 import com.example.steamprototype.data_op.UserLibraryStorage;
 import com.example.steamprototype.entity.Game;
 import com.example.steamprototype.entity.User;
-import com.smarteist.autoimageslider.SliderView;
 
 import java.util.ArrayList;
 
-public class WishListActivity extends AppCompatActivity {
+public class LibraryActivity  extends AppCompatActivity {
     Button btn_search;
     EditText edt_search;
     ListView listView;
@@ -28,28 +25,31 @@ public class WishListActivity extends AppCompatActivity {
     User user;
     UserLibraryStorage userLibraryStorage;
     public static final int REQUEST_CODE_BUY = 30;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.wishlist_activity);
+        setContentView(R.layout.library_activity);
         init();
         Intent intent = getIntent();
         this.user = (User) intent.getSerializableExtra("user");
 
-        listViewAdapter = new ListViewAdapter(WishListActivity.this, gameArrayList);
+        listViewAdapter = new ListViewAdapter(this, gameArrayList);
         listView.setAdapter(listViewAdapter);
+
         listView.setOnItemClickListener((parent, view, position, id) -> {
             gotoGamePage(position);
         });
-    }
-    public void init() {
-        btn_search = (Button) findViewById(R.id.btn_wishsearch);
-        edt_search = (EditText) findViewById(R.id.edit_wishsearch);
-        listView = (ListView) findViewById(R.id.lstWishlist);
+
+
+    } public void init() {
+        btn_search = (Button) findViewById(R.id.btn_librarysrch);
+        edt_search = (EditText) findViewById(R.id.edit_librarysrch);
+        listView = (ListView) findViewById(R.id.lstlibrary);
     }
     public void gotoGamePage(int pos) {
         Game game = gameArrayList.get(pos);
-        Intent buyIntent = new Intent(WishListActivity.this, GamePageActivity.class);
+        Intent buyIntent = new Intent(LibraryActivity.this, GamePageActivity.class);
         buyIntent.putExtra("game", game);
         startActivityForResult(buyIntent, REQUEST_CODE_BUY);
     }
